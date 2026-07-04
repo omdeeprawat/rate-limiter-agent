629e96c0c407fd9378b412908908a86b8aab4268
omdeeprawat <omdeepr91@gmail.com>
2026-07-04 06:39:47 +0530

    add: created SEttings class with env config

---
+++ rate_limiter/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+  redis_url : str = "redis://localhost:6379"
+  max_tokens : int = 10
+  refill_rate : int = 2
+  interval : float = 1.0
+  key_ttl : int = 3600
+
+  model_config = {"env_file": ".env", "env_file_encoding" : "utf-8"}
+
+settings = Settings()
